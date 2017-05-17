96cc8c78694d9a242a1e4b0bc0fff7a05ad44bea
Bauuuuu <abansode@outlook.com>
2017-05-17 12:30:36 +0200

    Revert "sched: Fix bug in average nr_running and nr_iowait calculation"
    This reverts commit 618a89a1e8d7a75d396528b79554a8fb22d9e1cf.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1346,7 +1346,7 @@ static inline u64 steal_ticks(u64 steal)
 
 static inline void inc_nr_running(struct rq *rq)
 {
-	sched_update_nr_prod(cpu_of(rq), 1, true);
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
 	rq->nr_running++;
 
 
@@ -1369,7 +1369,7 @@ if (rq->nr_running >= 2) {
 
 static inline void dec_nr_running(struct rq *rq)
 {
-	sched_update_nr_prod(cpu_of(rq), 1, false);
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
 	rq->nr_running--;
 }
 
